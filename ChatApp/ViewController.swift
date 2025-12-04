1f2a0a0df957eea92e4032012113b33e1547cad4
Elif Çağıl <elif.cagil@sans-technology.com>
2025-12-04 09:43:02 +0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Elif Çağıl on 4.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
